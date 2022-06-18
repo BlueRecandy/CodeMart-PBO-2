b2e28c33039f99e58006b27b8d66702b8b258bdf
TiveCS <creativecode.id@gmail.com>
2022-06-18 11:52:41 +0700

    add createUser and clean getUserById

--- CodeMart-PBO-2/src/io/bluerecandy/codemart/gui/service/UsersService.java
+++ CodeMart-PBO-2/src/io/bluerecandy/codemart/gui/service/UsersService.java
@@ -1,6 +1,13 @@
 package io.bluerecandy.codemart.gui.service;
 
+import io.bluerecandy.codemart.gui.model.Account;
 import io.bluerecandy.codemart.gui.model.User;
+import io.bluerecandy.codemart.gui.sql.SQLConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UsersService {
 
@@ -11,14 +18,44 @@ public class UsersService {
         return instance;
     }
 
+    private final static String SELECT_USER_BY_ID = "SELECT * FROM users WHERE id = ?;";
+
+    private final static String INSERT_USER_REGISTER = "INSERT INTO users(id, name) VALUES(?, ?);";
+
     private UsersService(){}
 
 
     public User getUserById(int id){
         User found = null;
-        // TODO find user by id in db
+        Connection connection = SQLConnector.getInstance().connect();
+        try {
+            PreparedStatement statement = connection.prepareStatement(SELECT_USER_BY_ID);
+            statement.setInt(1, id);
+
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()){
+                found = processUserResultSet(rs);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
         return found;
+    }
+
+    public boolean createUser(int id, String name){
+        Connection connection = SQLConnector.getInstance().connect();
+        try {
+            PreparedStatement statement = connection.prepareStatement(INSERT_USER_REGISTER);
+            statement.setInt(1, id);
+            statement.setString(2, name);
+
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public void setUserName(int id, String newName){
@@ -53,6 +90,22 @@ public class UsersService {
             }
         }
         return false;
+    }
+
+    private User processUserResultSet(ResultSet rs) throws SQLException {
+        int userId = rs.getInt(1);
+        String name = rs.getString(2);
+        int coin = rs.getInt(3);
+
+        Account account = AccountsService.getInstance().getAccountById(userId);
+
+        User user = new User();
+        user.setId(userId);
+        user.setName(name);
+        user.setCoin(coin);
+        user.setAccount(account);
+
+        return user;
     }
 
 }
